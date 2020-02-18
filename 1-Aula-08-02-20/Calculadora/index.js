25f548c6cc556de3af7b7cab70a093ce523ed0c0
RenatoShishido <renato.re2012@hotmail.com>
2020-02-18 12:24:19 -0400

    Calculator
    	*solved bug 0000001
    	*solved bug 10+15
    	*create method historico
    
    	*the percentage remains to be implemented

--- 1-Aula-08-02-20/Calculadora/index.js
+++ 1-Aula-08-02-20/Calculadora/index.js
@@ -1,13 +1,86 @@
-
-
 function Clear() {
-    document.getElementById('texto').value = "";
-    array = [];
+    if(document.calc.visor.value === '0'){
+        document.getElementById('texto').value = "";
+        array = [];
+    }else {
+        document.getElementById('texto').value = "";
+        array = [];
+        document.calc.visor.value = '0';
+    }
 }
 const clear = document.getElementById('C');
 clear.onclick = Clear;
 var array = [];
-function insert(num){
+var array2 = [];
+
+function pegavalor() {
+    const valor = document.calc.visor.value;
+    array.push(valor);
+    console.log(array);
+    document.getElementById('texto').value = "";
+}
+function Historico(){
+    document.getElementById('historico').value = array[0] + array[1] + array[2];
+}
+
+function insert(num) {
+    if(num === '-'){
+        pegavalor();
+        array.push('-');
+    }else if(num === '/'){
+        pegavalor();
+        array.push('/');
+    }else if(num === 'X'){
+        pegavalor();
+        array.push('X');
+    }else if(num === '+'){
+        pegavalor();
+        array.push('+');
+    }else if( num === '='){
+        const valor = document.calc.visor.value;
+        array.push(valor);
+        Historico();
+            if(array[1] === '+'){
+                let total = parseInt(array[0]) + parseInt(array[2]);
+                document.getElementById('texto').value = total
+                array = [];
+                
+                console.log(array);
+            }else if(array[1] === 'X'){
+                let total = parseInt(array[0]) * parseInt(array[2]);
+                document.getElementById('texto').value = total
+                array = [];
+               
+            }else if(array[1] === '-'){
+                let total = parseInt(array[0]) - parseInt(array[2]);
+                document.getElementById('texto').value = total
+                array = [];
+                array.push(total);
+               
+            }else if(array[1] === '/'){
+                let total = parseInt(array[0]) / parseInt(array[2]);
+                document.getElementById('texto').value = total
+                array = [];
+               
+            }
+    }
+    else{
+        if(document.calc.visor.value === '0'){
+            Clear();
+            var salvo = document.calc.visor.value;
+            document.calc.visor.value = salvo + num;
+        }else{
+            var salvo = document.calc.visor.value;
+            document.calc.visor.value = salvo + num;
+        }
+    }
+
+
+
+    
+}
+
+function valor(num){
     array.push(num);
     console.log(array);
     if(num === '='){
@@ -39,6 +112,5 @@ function insert(num){
     }else{
         document.getElementById('texto').value = num;
     }
-   
 }
 
